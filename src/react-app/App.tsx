1a09c24ca8da31019ce3e2012338fc5b8d4db1b0
z2c <zzc.cn@icloud.com>
2025-09-29 11:19:16 +0800

    去除 demo

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -6,7 +6,6 @@ import PricingPage from "@/pages/pricing";
 import BlogPage from "@/pages/blog";
 import AboutPage from "@/pages/about";
 import TonePage from "@/pages/tone"
-import AppPage from "@/demo/App";
 
 function App() {
     return (
@@ -18,7 +17,6 @@ function App() {
             <Route element={<PricingPage />} path="/pricing" />
             <Route element={<BlogPage />} path="/blog" />
             <Route element={<AboutPage />} path="/about" />
-            <Route element={<AppPage />} path="/app" />
         </Routes>
     );
 }
